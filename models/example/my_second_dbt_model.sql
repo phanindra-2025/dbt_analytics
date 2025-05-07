437a12249192ea860f872e5a0b265e6983547b3d
phanindra.pabbisetty@modak.com <phanindra.pabbisetty@modak.com>
2025-05-07 03:19:01 +0000

    update 2nd model

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,2 +1,7 @@
+
 -- Use the `ref` function to select from other models
-select * from {{ ref("my_first_dbt_model") }} where id = 1
+{{ config(materialized='view') }}
+
+select *
+from {{ ref('my_first_dbt_model') }}
+where id = 1
