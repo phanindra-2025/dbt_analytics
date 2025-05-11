6777b4fba1f08eb1f7258bda5d42d123988e7b53
phanindra.pabbisetty@modak.com <phanindra.pabbisetty@modak.com>
2025-05-11 05:05:07 +0000

    Re-org th eproject

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,37 +1,24 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
 with customers as (
-
     select * from {{ ref('customer') }}
-    -- select 
-    -- C_CUSTKEY as customer_id,
-    -- C_NAME as customer_name
-    -- from 
-    -- snowflake_sample_data.tpch_sf10.customer
-
 ),
-
 orders as ( 
-
-select * from {{ ref('orders') }}
-
+    select * from {{ ref('orders') }}
 ),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
     group by 1
-
 ),
-
 final as (
-
     select
         customers.customer_id,
         customers.customer_name,
@@ -39,11 +26,7 @@ final as (
         customer_orders.most_recent_order_date,
         coalesce (customer_orders.number_of_orders, 0) 
         as number_of_orders
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
-
 select * from final
