95d94c3b434cef627db7cf29f33713821a095276
phanindra.pabbisetty@modak.com <phanindra.pabbisetty@modak.com>
2025-05-11 04:44:26 +0000

    basuc dependency model

---
+++ models/orders.sql
@@ -0,0 +1,8 @@
+
+    select 
+    O_ORDERKEY as order_id,
+    O_CUSTKEY as customer_id,
+    O_ORDERDATE as order_date,
+    O_ORDERSTATUS as status
+    from 
+    snowflake_sample_data.tpch_sf10.orders
